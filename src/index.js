fcf0d5e7c81ef6b42ad65e5a9c8fdf24a545e3b1
Abel Yang <2529719324@qq.com>
2019-02-25 17:36:07 +0800

    one

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const appState ={
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    title:{
+    text:'React.js',
+    color:'red'
+    },
+    content:{
+        text:'React.js content',
+        color:'blue'
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
+function renderApp (appState) {
+    renderTitle(appState.title)
+    renderContent(appState.content)
+}
+
+function renderTitle (title) {
+    const titleDOM = document.getElementById('title')
+    titleDOM.innerHTML = title.text
+    titleDOM.style.color = title.color
+}
+
+function renderContent (content) {
+    const contentDOM = document.getElementById('content')
+    contentDOM.innerHTML = content.text
+    contentDOM.style.color = content.color
+}
+
+function dispatch (action) {
+    switch (action.type) {
+        case 'UPDATE_TITLE_TEXT':
+            appState.title.text = action.text
+            break
+        case 'UPDATE_TITLE_COLOR':
+            appState.title.color = action.color
+            break
+        default:
+            break
+    }
+}
+renderApp(appState)
+//抽离
+function creact() {
+    
+}
